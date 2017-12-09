c4d202a73346c7b4c175568a131a2f467af32d06
David Brieden <davidbrieden@aol.com>
2017-12-09 23:35:10 +0100

    Zwischenstand

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var updatefolder = '';
 var updates = [];
 
 var scripts = {};
+// Erzeugt gegebenfalls die Updatescript-Tabelle und gibt den aktuellen Stand zurück
 scripts.create_t_updatescripts =
 `
 CREATE TABLE IF NOT EXISTS \`test\`.\`t_updatescripts\` (
@@ -20,6 +21,25 @@ select
   coalesce(max(t.updatescriptid), 0) as updatescriptid
 from t_updatescripts t;
 `;
+
+var initModule = function(param) {
+
+    var configFilename = path.resolve(process.cwd(), param);
+    var config = require(configFilename);
+
+    updatefolder = path.resolve(process.cwd(), config.updatefolder);
+    
+    updates = fs.readdirSync(updatefolder);
+    updates = updates.sort();
+
+    initConnection({
+        host: config.host,
+        user: config.user,
+        password: config.password,
+        database: config.database,
+        multipleStatements: true
+    });
+};
 
 var initConnection = function(config) {
     connection = mysql.createConnection(config);
@@ -35,10 +55,19 @@ var initConnection = function(config) {
 };
 
 var execUpdate = function(updatescriptid) {
-    var scriptname = updates.find( name => new RegExp(`^update0{0,3}${updatescriptid}_\\w*\\.sql$`, 'g').test(name))
+    var scriptname = updates.find( name => new RegExp(`^update0{0,3}${updatescriptid}\\w*\\.sql$`, 'g').test(name))
     console.log(`spiele Script ${updatescriptid} (${scriptname}) ein...`);
 };
 
+var getUpdatescriptNumber = function(scriptname) {
+    var result = (/^update(\d{4})\w+\.sql$/g).exec(scriptname);
+    if (result.length == 0 || isNaN(parseInt(result[1])) ) return -1;
+    else return result[1];
+};
+
+/*******************************************************************************
+ Module Exports
+ ******************************************************************************/
 
 var update = function() {
 
@@ -47,6 +76,24 @@ var update = function() {
     connection.query(scripts.create_t_updatescripts, function (error, results, fields) {
         if (error) throw error;
         var akt_updateid = results[1][0].updatescriptid;
+
+        var last_updatescriptid = 0;
+        var updatescriptid;
+        // iteriert durch die vorhanden Updatescripte
+        // nimmt vergangene Scripte aus der Betrachtung
+        // findet doppelte Scripte -> Fehler
+        for (var i = updates.length - 1; i >= 0; i--) {
+            updatescriptid = getUpdatescriptNumber(updates[i]);
+
+            if (updatescriptid == -1) {
+                console.log();
+            }
+
+            if (updatescriptid <= akt_updateid)
+                updates.splice(i, 1);
+            
+        }
+
 
         if(akt_updateid === updates.length) {
             console.log("Alle Updates eingespielt!");
@@ -60,21 +107,7 @@ var update = function() {
 
 
 module.exports = function(param){
-
-    var configFilename = path.resolve(path.dirname(module.parent.filename), param);
-    var config = require(configFilename);
-
-    updatefolder = path.resolve(path.dirname(module.parent.filename), config.updatefolder);
-    
-    updates = fs.readdirSync(updatefolder);
-
-    initConnection({
-        host: config.host,
-        user: config.user,
-        password: config.password,
-        database: config.database,
-        multipleStatements: true
-    });
+    initModule(param);
 
     return {
         update
